a2ae36b5bec83c3a30f1b94c16d47cd2a6d70ce8
Philipp Kähler <p.kaehler@mytaxi.com>
2017-03-29 11:51:26 +0200

    added history and changed some styles

--- app/src/main/java/com/baecon/rockpaperscissorsapp/MainActivity.java
+++ app/src/main/java/com/baecon/rockpaperscissorsapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView options = (TextView) findViewById(R.id.options);
+        TextView history = (TextView) findViewById(R.id.history);
+        history.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent historyIntent = new Intent(MainActivity.this, HistoryActivity.class);
+               startActivity(historyIntent);
+           }
+       });
 
+        TextView options = (TextView) findViewById(R.id.options);
         options.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent optionsIntent = new Intent(MainActivity.this, Options.class);
+                Intent optionsIntent = new Intent(MainActivity.this, OptionActivity.class);
                 startActivity(optionsIntent);
             }
         });
--- app/src/main/java/com/baecon/rockpaperscissorsapp/OptionActivity.java
+++ app/src/main/java/com/baecon/rockpaperscissorsapp/OptionActivity.java
@@ -3,12 +3,12 @@ package com.baecon.rockpaperscissorsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Options extends AppCompatActivity {
+public class OptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.options);
+        setContentView(R.layout.activity_options);
 
         // Get the Intent that started this activity and extract the string
 //        Intent intent = getIntent();
