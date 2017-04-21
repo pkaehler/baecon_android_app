c3dbc76fc79276e6a5290c2ab78fd12115e934df
Philipp Kähler <philipp.kaehler@googlemail.com>
2017-04-21 17:14:25 +0200

    some error handling

--- app/src/main/java/com/baecon/rockpaperscissorsapp/activity/HistoryActivity.java
+++ app/src/main/java/com/baecon/rockpaperscissorsapp/activity/HistoryActivity.java
@@ -1,5 +1,7 @@
 package com.baecon.rockpaperscissorsapp.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +10,14 @@ import android.widget.ListView;
 
 import com.baecon.rockpaperscissorsapp.adapter.ResultAdapter;
 import com.baecon.rockpaperscissorsapp.R;
+import com.baecon.rockpaperscissorsapp.model.ReturnedErrorMessage;
 import com.baecon.rockpaperscissorsapp.model.Stats;
 import com.baecon.rockpaperscissorsapp.rest.ApiClient;
 import com.baecon.rockpaperscissorsapp.rest.ApiInterface;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import retrofit2.Call;
@@ -41,18 +47,37 @@ public class HistoryActivity extends AppCompatActivity {
 
 
     public void getLastGames(int id){
-        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        final ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
         Call<Stats> call = apiService.getStats(id);
         call.enqueue(new Callback<Stats>() {
             @Override
             public void onResponse(Call<Stats> call, Response<Stats> response) {
+                if (response.code() == 400) {
+                    Gson gson = new GsonBuilder().create();
+                    ReturnedErrorMessage errorMessage = new ReturnedErrorMessage();
+
+                    try {
+                        errorMessage = gson.fromJson(response.errorBody().string(),ReturnedErrorMessage.class);
+                        new AlertDialog.Builder(HistoryActivity.this)
+                                .setTitle("Loot upassen")
+                                .setMessage("something went wrong. Got: " + errorMessage.getErrorCode() + " with message " + errorMessage.getErrorMessage())
+                                .setNegativeButton(R.string.cancel_label, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                })
+                                .show();
+                        Log.d(TAG,"Fehler: " + response.body());
+                        return;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
                 Stats resource = response.body();
-                int rocks = resource.getRockCount();
-                int papers = resource.getPaperCount();
-                int scissors = resource.getScissorCount();
-                Log.d(TAG,resource.toString());
-                Log.d(TAG,"Name in Ressource: " + resource.getUser().getName());
+//                  Log.d(TAG,resource.toString());
+//                  Log.d(TAG,"Name in Ressource: " + resource.getUser().getName());
 
                 adapter.clear();
                 adapter.addAll(resource);
